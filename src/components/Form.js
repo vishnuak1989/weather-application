be9ded246ff7cc88c8c2f71f0081c79a975750c4
Vishnu Anilkumar <vishnuak1989@gmail.com>
2018-06-01 14:39:06 -0400

    Mobile Responsivness added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,23 @@
 import React from "react";
 
 const Form = props =>(
+  <div className="formComponent">
   <form onSubmit={props.getWeather} className="form-inline">
     <div className="input-group">
       <div className="input-group-prepend">
-        <span className="input-group-text" id="">City and Country</span>
+        <span className="input-group-text" id="">City & Country</span>
       </div>
-      <input type="text" name="city" placeholder="Enter name of the city" className="form-control"></input>
-      <input type="text" className="form-control" name="country" placeholder="Enter name of the country"></input>
+      <div className="forminput">
+      <input type="text" name="city" placeholder="City Name" className="form-control"></input>
+      </div>
+      <div className="forminput">
+      <input type="text" className="form-control" name="country" placeholder="Country Name/Code"></input>
+      </div>
+      <div>
       <button className="btn btn-secondary" type="submit">Get Weather</button>
     </div>
+    </div>
   </form>
+  </div>
 );
 export default Form;
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,8 @@ class Titles extends React.Component {
   render(){
     return(
       <div className="titles"> 
-<h1>Weather application</h1>
-<p>Find out weather of your favourite city</p>
+      <h1>Weather application</h1>
+      <p>Find out weather of your favourite city</p>
       </div>
       
     );
